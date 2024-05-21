4461f1db7e3c0e60967651036be0b2a30f7a34c1
GaoJ <gj37604@163.com>
2024-05-22 00:58:50 +0800

    部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,10 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
+  base: '/jay-blog',
   // 请不要忘记设置默认语言
   lang: 'zh-CN',
   theme: plumeTheme({
-    // more...
   }),
   bundler: viteBundler(),
 })
